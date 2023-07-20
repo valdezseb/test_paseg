91302afe89a2e4945d31f80f9f782a1de22fd0e0
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-20 15:55:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
 import openai
 import os
-from langchain.chat_models import ChatOpenAI
 
 from langchain.vectorstores import Chroma, Pinecone
 import pinecone
@@ -18,32 +17,48 @@ pinecone.init(api_key=api_key, environment='asia-southeast1-gcp-free')
 index_name = 'dbpaseg'
 
 
-# Define username and password
-username = "ppca"
-password = "65326"
 
-# Define Streamlit app
-st.set_page_config(page_title="PASEG Genie // buy me a coffee", page_icon=":coffee:")
+# Prompt user for OpenAI API key
 
-st.title("PASEG Genie ")
+# Prompt user for OpenAI API key
+openai_api_key = st.text_input("Enter your OpenAI API key:")
+if openai_api_key:
+    os.environ['OPENAI_API_KEY'] = openai_api_key
 
-login_username = st.text_input("Username:")
-login_password = st.text_input("Password:", type="password")
-openai_api_key = st.text_input("OpenAI API key:")
-if st.button("Login"):
-    if login_username == username and login_password == password and openai_api_key:
-        st.success("Login successful!")
-        os.environ['OPENAI_API_KEY'] = openai_api_key
-        embeddings = HuggingFaceEmbeddings()
-        docsearch = Pinecone.from_existing_index(index_name, embeddings)
-        chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
-        qachain = load_qa_chain(chat, chain_type='stuff')
-        qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
-        condition1 = '\n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
-        query = st.text_input("Enter your query:")
-        if st.button("Get answer"):
-            q = query + '\n' + condition1
-            result = qa.run(q)
-            st.write(result)
-    else:
-        st.error("Invalid username, password, or OpenAI API key.")
+
+
+
+
+embeddings = HuggingFaceEmbeddings()
+
+# if you already have an index, you can load it like this
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+
+chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
+
+qachain = load_qa_chain(chat, chain_type='stuff')
+
+qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
+
+condition1 = '\n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
+
+# INTEGRATE STREAMLIT TO THE FOLLOWING CODE, SO USER CAN INPUT THE QUERY, AND RESULT SHOULD A APPEAR IN A TEXT BOX USING STREAMLIT
+
+
+
+st.title("Question Answering System")
+
+
+
+query = st.text_input("Enter your query:")
+
+if st.button("Get answer"):
+    q = query + '\n' + condition1
+    result = qa.run(q)
+    st.write(result)
