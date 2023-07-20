52cf56860617250b59c2b15c263de5490d8e6a84
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-20 15:34:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ password = "65326"
 # Define Streamlit app
 st.set_page_config(page_title="PASEG Genie // buy me a coffee", page_icon=":coffee:")
 
-st.title("PASEG Genie ")
+st.title("PASEG Genie " + emoji.emojize(":coffee:"))
 
 login_username = st.text_input("Username:")
 login_password = st.text_input("Password:", type="password")
@@ -36,16 +36,16 @@ if st.button("Login"):
             openai_api_key = st.text_input("Enter your OpenAI API key:")
             if openai_api_key:
                 os.environ['OPENAI_API_KEY'] = openai_api_key
-        embeddings = HuggingFaceEmbeddings()
-        docsearch = Pinecone.from_existing_index(index_name, embeddings)
-        chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
-        qachain = load_qa_chain(chat, chain_type='stuff')
-        qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
-        condition1 = '\n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
-        query = st.text_input("Enter your query:")
-        if st.button("Get answer"):
-            q = query + '\n' + condition1
-            result = qa.run(q)
-            st.write(result)
+                embeddings = HuggingFaceEmbeddings()
+                docsearch = Pinecone.from_existing_index(index_name, embeddings)
+                chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
+                qachain = load_qa_chain(chat, chain_type='stuff')
+                qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
+                condition1 = '\n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
+                query = st.text_input("Enter your query:")
+                if st.button("Get answer"):
+                    q = query + '\n' + condition1
+                    result = qa.run(q)
+                    st.write(result)
     else:
         st.error("Invalid username or password.")
