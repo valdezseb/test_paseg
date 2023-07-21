0e791ca331475fdee535a1647d7e792a33d19884
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-21 06:07:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,12 +85,3 @@ if q:
   result = qa.run(q)
   st.write(result)
 
-#hide_st_style = """
-#            <style>
-#            MainMenu {visibility: hidden;}
-#            footer {visibility: hidden;}
-#            header {visibility: hidden;}
-#            </style>
-#            """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
-
