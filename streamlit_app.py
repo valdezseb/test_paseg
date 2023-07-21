e28e51e7ab6d4b99d26522a202285ca383050824
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-21 06:13:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ api_key = st.secrets["pinecone_api_key"]
 pinecone.init(api_key=api_key, environment='asia-southeast1-gcp-free')
 index_name = 'dbpaseg'
 
-os.environ['OPEN_API_KEY'] = st.secrets['openapikey']
+os.environ['OPENAI_API_KEY'] = st.secrets['openai_api_key']
 
 # Define username and password
 username = "ppca"
