0e6802337bc0254bdf5bfc4ec51b488335e37f1d
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-24 10:57:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,11 @@ import pinecone
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
 
 #st.set_page_config(page_title="PASEG Genie ", page_icon=":coffee:")
 st.set_page_config(page_title="PASEG Genie ", page_icon=":coffee:", layout="wide")
@@ -65,7 +70,7 @@ chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
 qachain = load_qa_chain(chat, chain_type='stuff')
 qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
 
-condition1 = '\n [Generate Response/Text from my data.] \n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
+condition1 = '\n [Generate Response/Text from my data.] \n [if applicable for the query add section: Things to Promote/Things to Avoid and Best Practices, Examples] \n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
 
 st.title("PASEG Genie // for education purpose :coffee:")
 #st.markdown("Donate a coffee")
