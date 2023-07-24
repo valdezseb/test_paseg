d340d4262eaa5cda99f43e67aaa0af9ba32a5752
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-24 10:18:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ docsearch = load_embeddings_and_pinecone()
 chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
 qachain = load_qa_chain(chat, chain_type='stuff')
 qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
-condition1 = '\n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
+
+condition1 = '\n [Generate Response/Text from my data.] \n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
 
 st.title("PASEG Genie // for education purpose :coffee:")
 #st.markdown("Donate a coffee")
