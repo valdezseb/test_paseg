a1439ed499f219e219b48ee97b4f3b688ba00cbb
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-25 09:52:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,169 @@ st.markdown("Data Analysis Section")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file, engine="openpyxl")
+    # Split the Predecessors column by comma and join the values that contain a special character or word
+    df['task_dependency'] = df['Predecessors'].apply(lambda x: ','.join([y for y in str(x).split(',') if any(c.isalpha() for c in y)]))
+    
+    def clean_string(s):
+        # Split the string by ","
+        s = s.split(",")
+    
+        # Remove letters and special characters from each substring
+        s = [re.sub(r"[^\d+]", "", x) for x in s]
+    
+        # Remove the number followed by "+" in each substring
+        s = [re.sub(r"\+\d+", "", x) for x in s]
+    
+        # Convert each non-empty substring to an integer
+        s = [int(x) for x in s if x]
+    
+        return s
+    
+    # Apply the function to each row of the "Task Dependencies" column and create a new column "task_dependency"
+    df["task_dependency_ids"] = df["task_dependency"].apply(clean_string)
+    
+    def get_max_dependency_dates(df):
+        # Step 1: Create a dictionary that maps each unique ID to its start and finish dates
+        id_to_dates = dict(zip(df["Unique_ID"], zip(df["Start_Date"], df["Finish_Date"])))
+    
+        # Step 2: Iterate over each row of the DataFrame and find the max dependency start and finish dates
+        max_dependency_starts = []
+        max_dependency_finishes = []
+        for i, row in df.iterrows():
+            # Get the task_dependency value
+            dependency_str = row["task_dependency"]
+    
+            # Check if the task_dependency contains "SS" or "SF"
+            if "SS" in dependency_str or "SF" in dependency_str:
+                # Get the task_dependency_ids value
+                dependency_ids = row["task_dependency_ids"]
+    
+                # Find the start and finish dates for each dependency ID using the id_to_dates dictionary
+                dependency_dates = [id_to_dates[id] for id in dependency_ids if id in id_to_dates]
+    
+                # Calculate the max start and finish dates from the dependency IDs
+                if dependency_dates:
+                    max_dependency_start = min([d[0] for d in dependency_dates])
+                    max_dependency_finish = max([d[1] for d in dependency_dates])
+                else:
+                    max_dependency_start = row["Start_Date"]
+                    max_dependency_finish = row["Finish_Date"]
+            else:
+                max_dependency_start = ""
+                max_dependency_finish = ""
+    
+            # Store the max start and finish dates in new columns of the DataFrame
+            max_dependency_starts.append(max_dependency_start)
+            max_dependency_finishes.append(max_dependency_finish)
+    
+        df["max_dependency_start"] = max_dependency_starts
+        #df["max_dependency_finish"] = max_dependency_finishes
+    
+        return df
+    
+    df = get_max_dependency_dates(df)
+    
+    
+    # Step 1: Create a dictionary that maps each unique ID to its finish date
+    id_to_finish = dict(zip(df["Unique_ID"], df["Finish_Date"]))
+    
+    # Step 2: Iterate over each row of the DataFrame and find the max dependency finish date
+    max_dependency_finishes = []
+    for i, row in df.iterrows():
+        # Check if the task_dependency contains "FF"
+        if "FF" in row["task_dependency"]:
+            # Get the task_dependency_ids value
+            dependency_ids = row["task_dependency_ids"]
+    
+            # Find the finish date for each dependency ID using the id_to_finish dictionary
+            dependency_finishes = [id_to_finish[id] for id in dependency_ids if id in id_to_finish]
+    
+            # Calculate the max finish date from the dependency IDs
+            if dependency_finishes:
+                max_dependency_finish = max(dependency_finishes)
+            else:
+                max_dependency_finish = row["Start_Date"]
+        else:
+            max_dependency_finish = ""
+    
+        # Store the max finish date in a new column of the DataFrame
+        max_dependency_finishes.append(max_dependency_finish)
+    
+    df["max_dependency_finish"] = max_dependency_finishes
+    
+    # Convert the "max_dependency_start" and "max_dependency_finish" columns to datetime format
+    df["max_dependency_start"] = pd.to_datetime(df["max_dependency_start"]).dt.date
+    df["max_dependency_finish"] = pd.to_datetime(df["max_dependency_finish"]).dt.date
+    
+    # Remove the brackets from the "a" column
+    df["task_dependency_ids"] = df["task_dependency_ids"].astype(str).str.replace("[", "").str.replace("]", "");
+    
+    df["new_task_dependency"] = np.where(df["task_dependency"] == "", "No Dependency",
+                                          np.where(df["task_dependency"] == "nan", "No Predecessors",
+                                                   df["task_dependency"]))
+    
+    # Define a regular expression to extract only numbers and commas
+    pattern = r'\b(\d+)(?:,|$)'
+    
+    # Apply the regular expression to the "predecessors" column and extract all matches
+    df['Predecessors_clean'] = df['Predecessors'].str.extractall(pattern).groupby(level=0).agg(','.join)
+    
+    # Define a function to compute the task names for each row
+    def compute_predecessor_names(row):
+        predecessors = row['Predecessors_clean']
+        if pd.isna(predecessors):
+            return ''
+        else:
+            predecessors = set(map(int, predecessors.split(',')))
+            predecessor_names = list(df.loc[df['Unique_ID'].isin(predecessors), 'Task_Name'])
+            return ','.join(predecessor_names)
+    
+    # Apply the function to each row and create a new column
+    df['Critical_Path'] = df.apply(compute_predecessor_names, axis=1)
+    
+    #time stamp for current time
+    now = dt.datetime.now()
+    
+    #Prepare datetime objects for further data analysis
+    df['Start_Date'] = pd.to_datetime(df['Start_Date'].apply(lambda x: x.strftime('%b %d, %Y')))
+    df['Finish_Date'] = pd.to_datetime(df['Finish_Date'].apply(lambda x: x.strftime('%b %d, %Y')))
+    
+    #Prepare datetime objects for further data analysis
+    df['max_dependency_start'] = df['max_dependency_start'].apply(lambda x: x.strftime('%b %d, %Y') if pd.notna(x) else None)
+    df['max_dependency_finish'] = df['max_dependency_finish'].apply(lambda x: x.strftime('%b %d, %Y') if pd.notna(x) else None)
+    
+    
+    
+    #Convert Duration to days
+    df['Duration'] = pd.to_timedelta(df['Duration'], errors='coerce')
+    df['Duration'] = df['Duration'].dt.days
+    
+    #Create Time to Finish
+    df['Time to Finish'] = df['Finish_Date'] - now
+    df['Time to Finish'] = df['Time to Finish'].dt.days
+    
+    #Create Time to Start
+    df['Time to Start'] = df['Start_Date'] - now
+    df['Time to Start'] = df['Time to Start'].dt.days
+    
+    conditions = [
+        (df['Finish_Date'] <= now) & pd.isna(df['Actual_Finish']),
+         (df['Start_Date'] <= now) & pd.isna(df['Actual_Start']),
+        (df['Finish_Date'] >= now) & (df['Actual_Start'] <= now),
+        ~pd.isna(df['Actual_Finish']),
+        (df['Start_Date'] >= now) & (pd.isna(df['Actual_Start']))
+    ]
+    values = ['Should Finished', 'Should Started','In Progress', 'Completed', 'Future Tasks']
+    
+    # use np.select() to create the 'Status' column
+    df['Status'] = np.select(conditions, values, default='Delayed')
+    
+    #convert dt to days
+    df['Total_Slack_Days'] = pd.to_timedelta(df['Total_Slack'], errors='coerce').dt.days
+    
+    #Create/Calculate Total Predecessors
+    df['Total Predecessors'] = df['Predecessors'].apply(lambda x: len(x.split(',')) if isinstance(x, str) else None)
+
     # Generate the HTML using Pygwalker
     #pyg_html = pyg.walk(df, return_html=True)  
     # Embed the HTML into the Streamlit app
