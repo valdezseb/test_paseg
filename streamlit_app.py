2fdf807cb5fb892f3e9d226bde96898bccbf4527
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-25 09:12:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ st.markdown("Data Analysis Section")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     
-    df = pd.read_excel(uploaded_file)
+    df = pd.read_excel(uploaded_file, engine="openpyxl")
     st.write(df)
     # Generate the HTML using Pygwalker
     pyg_html = pyg.walk(df, return_html=True)  
